53644111301931bc6334447b18e4e7fccecfb40e
MAOZ <maooz4426@gmail.com>
2025-04-14 16:43:31 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-
-
 export default function RootLayout({
   children,
 }: Readonly<{
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,6 @@
 import {useEffect} from "react";
 
 export default function Home() {
-  const a = 0
-  useEffect(() => {
-    console.log(a)
-  }, [a]);
+
   return <div>a</div>;
 }
