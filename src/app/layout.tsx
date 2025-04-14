20b605401bd4870dcfc9c672f90b939d99055aad
MAOZ <maooz4426@gmail.com>
2025-04-14 16:56:01 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,13 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
 }
