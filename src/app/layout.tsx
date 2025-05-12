70c3e9a26da5d01ca4cfab50729b4ff1ccfe399d
MAOZ <maooz4426@gmail.com>
2025-05-12 14:14:37 +0900

    pandaのicon修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 		</head>
 		<body className="max-h-screen bg-green-50">
 		<div className="p-4 w-full flex items-center justify-center">
-			<Image src="/panda.png" height={40} width={40} alt="Pandatorアイコン" className="mr-2"/>
+			<Image src="/icon/panda.png" height={40} width={40} alt="Pandatorアイコン" className="mr-2"/>
 			<h1 className="text-center text-3xl text-green-600">Pandator</h1>
 		</div>
 		{children}
